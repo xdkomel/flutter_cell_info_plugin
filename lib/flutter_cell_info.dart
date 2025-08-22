df2dad71d47b1c89e820d150de55a7ec9d56456b
Kamil Foatov <xdkomel@gmail.com>
2025-08-22 19:17:34 +0300

    patches for ios

--- lib/flutter_cell_info.dart
+++ lib/flutter_cell_info.dart
@@ -1,29 +1,39 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
 /// A class that provides information about the cell.
 class CellInfo {
   /// Returns a JSON string containing information about the cell.
-  static const MethodChannel _channel = const MethodChannel(
+  static const MethodChannel _channel = MethodChannel(
     'com.xdkomel.flutter_cell_info_plugin/method_channel',
   );
 
   /// Returns a JSON string containing information about the cell.
   static Future<String?> get getCellInfo async {
-    final String? version = await _channel.invokeMethod('cell_info');
-    return version;
+    if (Platform.isAndroid) {
+      final String? version = await _channel.invokeMethod('cell_info');
+      return version;
+    }
+    return null;
   }
 
   /// Returns a JSON string containing information about the SIM card.
   static Future<String?> get getSIMInfo async {
-    final String? version = await _channel.invokeMethod('sim_info');
-    return version;
+    if (Platform.isAndroid) {
+      final String? version = await _channel.invokeMethod('sim_info');
+      return version;
+    }
+    return null;
   }
 
   /// Returns a JSON string containing information about the shared preference.
   static Future<String?> get sharedPreference async {
-    final String? version = await _channel.invokeMethod('shared_preference');
-    return version;
+    if (Platform.isAndroid) {
+      final String? version = await _channel.invokeMethod('shared_preference');
+      return version;
+    }
+    return null;
   }
 }
